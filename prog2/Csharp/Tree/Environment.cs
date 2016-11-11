b8542ea287e3e42eac573c6eb3b4e8e065905cee
Amanda <amandahelenaalfaro@gmail.com>
2016-11-11 14:46:53 -0600

    Updating assign, define and lookup
    Add comments to help understand too.

--- prog2/Csharp/Tree/Environment.cs
+++ prog2/Csharp/Tree/Environment.cs
@@ -17,14 +17,15 @@
 //  - constructors:
 //      - create the empty environment (an environment with an empty frame)
 //      - add an empty frame to the front of an existing environment
-//  - lookup the value for a name (for implementing variable lookup)
+//  DONE- lookup the value for a name (for implementing variable lookup)
 //      if the name exists in the innermost scope, return the value
 //      if it doesn't exist, look it up in the enclosing scope
 //      if we don't find the name, it is an error
-//  - define a name (for implementing define and parameter passing)
+//  DONE- define a name (for implementing define and parameter passing)
 //      if the name already exists in the innermost scope, update the value
 //      otherwise add a name-value pair as first element to the innermost scope
-//  - assign to a name (for implementing set!)
+
+//  DONE- assign to a name (for implementing set!)
 //      if the name exists in the innermost scope, update the value
 //      if it doesn't exist, perform the assignment in the enclosing scope
 //      if we don't find the name, it is an error
@@ -72,15 +73,18 @@ namespace Tree
         // translation of the Scheme assq function.
         private static Node find(Node id, Node alist)
         {
+	    //if the association list is empty
             if (! alist.isPair())
                 return null;	// in Scheme we'd return #f
             else
             {
                 Node bind = alist.getCar();
+		//If the ID is in an association list
                 if (id.getName().Equals(bind.getCar().getName()))
                     // return a list containing the value as only element
                     return bind.getCdr();
                 else
+		
                     return find(id, alist.getCdr());
             }
         }
@@ -91,27 +95,60 @@ namespace Tree
             Node val = find(id, frame);
             if (val == null && env == null)
             {
-                Console.Error.WriteLine("undefined variable " + id.getName());
+                Console.Error.WriteLine("The variable " + id.getName() + "is undefined");
                 return null;
             }
             else if (val == null)
                 // look up the identifier in the enclosing scope
                 return env.lookup(id);
-            else
+            else	
                 // get the value out of the list we got from find()
 		return val.getCar();
         }
 
 
-        public void define(Node id, Node val)
-        {
-            // TODO: implement this function
+        public void define(Node id, Node val) {
+	
+            //defining a name
+            Node nameVal = find(id,frame); 
+	    //Checking to see if the name is in the innermost scope
+	    //if the name already exists in the innermost scope
+	    
+            if(nameVal != null)	{   
+	    	 //updating nameVal	
+		 value.setCar(val);
+  	    }	else {
+	    	//add a name-value pair as first element to the innermost scope
+		frame = new Cons(new Cons(id, new Cons(val, new Nil())), frame);
+            }
+	    
         }
 
 
-        public void assign(Node id, Node val)
-        {
-            // TODO: implement this function
+        public void assign(Node id, Node val) {
+	   //Going to use the lookup method to make the code more efficent
+	    /*Node value = find(id, frame); 
+	    
+	    //if the name exists in the innermost scope
+            if (value == null && env == null) 
+	    	//Update value
+	    	define(id, val); 
+		
+	    //if the name DOESNT exist in the innermost scope	
+            else if (value == null) 
+	    	//Perform the assignment in the enclosing scope
+                env.findForAssign(id, val);
+	    
+	    //If value isnt found
+            else
+	    	//Print undefined varibale error
+                Console.Error.WriteLine("The variable" + id.getName() + "is undefined"); */
+		
+		
+	   if (this.lookup(id) != null)	
+              find(id, this.frame).setCar(val);
+           else 
+              Console.Error.WriteLine("The varaible " + id.getName() + "is undefined");
         }
     }
 }
